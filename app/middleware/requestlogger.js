b6b2ab9c884d7ed524b620895b2f128c4a102bc6
timsupport9 <tymoomuthee@gmail.com>
2026-09-17 18:23:20 +0300

    Update requestlogger.js

--- app/middleware/requestlogger.js
+++ app/middleware/requestlogger.js
@@ -1,8 +1,17 @@
-module.exports = function requestLogger(req, res, next) {
-  const started = Date.now();
-  res.on("finish", () => {
-    const duration = Date.now() - started;
-    console.log(`${req.method} ${req.originalUrl} ${res.statusCode} ${duration}ms`);
-  });
-  next();
-};
+
+
+function requestLogger(req, res, next) {
+    const start = Date.now();
+
+    res.on("finish", () => {
+        const duration = Date.now() - start;
+
+        console.log(
+            `[HTTP] ${req.method} ${req.originalUrl} ${res.statusCode} ${duration}ms`
+        );
+    });
+
+    next();
+}
+
+module.exports = requestLogger;
